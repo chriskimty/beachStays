feb393004537dfde4253e737ccc52ae6d16a4184
chriskimty <christaeyoung.kim@gmail.com>
2022-09-25 21:36:24 -0400

    added JS functionality to contact form

--- README.md
+++ README.md
@@ -1,28 +1,15 @@
-# Project 1: Multi-page Site ("Beachstays")
+# Beachstays 2.0!: Adding Interactivity to Project 1 ("Beachstays") with JavaScript (Assignment 2)
 
 ## Description
 
-[Beachstays](https://papaya-alpaca-edc2a9.netlify.app/) is a multi-page PSD conversion project that utilizes HTML and SCSS to create a functional and responsive website, providing a good user-experience across multiple devices and screen sizes. 
+[Beachstays 2.0](https://AddNewLink.com) is an extension to [Project 1: Beachstays](papaya-alpaca-edc2a9.netlify.app/) where additional UI elements were added to the original design using JavaScript. 
 
-Three desktop PSD files were provided: homepage, blog, and contact page, as well as all the original assets/images. Since no mobile design was supplied, this project features a desktop-first responsive design, utilizing necessary breakpoints from large desktop to mobile (down to 320px). The project fulflils all the project requirements including uses of:
-* Media queries for responsiveness
-* Flexbox and wrapper
-* Sass variables
-* Accessibility (i.e. form labels, image alt tags)
-* Consistent naming convention
-* Organization of SCSS partials
-* Functional navigation
-* HTML and CSS best practices
+New elements include:
+* A functional hamburger menu with a slide-out navigation
+* A functional form with a submission confirmation text and refresh button
+* An image carousel
 
-N.B.: The project will be updated at a later date to include a functional hamburger menu using JavaScript. 
-
-This project was completed in September 2022 with [Juno College](https://junocollege.com/)'s Web Development Bootcamp Cohort 44.
-
-## Acknowledgments/Attributions
-* Icons from [FontAwesome](https://fontawesome.com/)
-* Icons from [Free Icons](https://freeicons.io/)
-* Fonts from [Google Fonts](https://fonts.google.com/)
-* [Box Shadow CSS Generator](https://cssgenerator.org/box-shadow-css-generator.html)
+This assignment was completed in September 2022 with [Juno College](https://junocollege.com/)'s Web Development Bootcamp Cohort 44.
 
 ## Contributor/Author
 
--- script.js
+++ script.js
@@ -1,24 +1,73 @@
-// Hide the hamburger menu on click
-// - on click, show the slide-out menu
-// - on click, hide the hamburger button (class navButton)
+// HAMBURGER MENU
+/* On the hamburger/nav button
+    - on click, show the slide-out menu
+    - on click, hide the hamburger button (class navButton) */
 
+/*  On the X button on the slide-out nav
+    - on click, hide the slide-out menu
+    - on click, hide the class of hamburger menu */
+
+// Defining Variables
 const hamElement = document.querySelector(".navButton");
 const slideOutMenu = document.querySelector(".slideOutNav");
 const hamCloseButton = document.querySelector(".xButton");
 
-// ES5 version
-// hamElement.addEventLister('click', function() {
-//code here
-// });
-
-// ES6
+// On click, open slide-out nav and remove hamburger icon
 hamElement.addEventListener("click", () => {
-    slideOutMenu.classList.add("slideOutNavOpen")
-    // what I target.object.method (add is exclusive to the classList). Classlist specifies that this is a class so you don't need the dot!
+    slideOutMenu.classList.add("slideOutNavOpen");
+    hamElement.style.display = "none";
+    /* personal note: 
+    what I target.object.method (add is exclusive to the classList). Classlist specifies that this is a class so you don't need the dot! */
 })
 
+// On click, close slide-out nav and show hamburger icon
+hamCloseButton.addEventListener("click", () => {
+    slideOutMenu.classList.remove("slideOutNavOpen");
+    hamElement.style.display = "block";
+})
 
+// CONTACT FORM 
+/* Upon submission of form (conditional - content should not be blank): 
+    - remove the contact form
+    - replace with confirmation message on screen (create)
+    - clear original values entered 
+    - create refresh button to return to original page
+*/
 
-// X button
-// - on click, hide the slide-out menu
-// - on click, hide the class of hamburger menu
+// Defining Variables 
+const submitForm = document.querySelector(".sendForm");
+const nameElement = document.querySelector("input");
+const emailElement = document.querySelector("input#email");
+const messageElement = document.querySelector("textarea");
+const contactForm = document.querySelector(".myForm");
+const refreshButton = document.querySelector(".refreshButton");
+
+// Function for contact form 
+contactForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (contactForm !== '') {
+        // Create div & h2 submission confirmation message
+        let div = document.createElement('div');
+        div.id = 'newText';
+        div.className = 'new';
+        let h2 = document.createElement('h2');
+        const fullName = nameElement.value;
+        h2.textContent = `Thank you for your message, ${fullName}. We will get back to you within 1-2 business days!`;
+        div.appendChild(h2);
+        const removedForm = document.querySelector(".fillForm");
+        removedForm.appendChild(div);
+
+    // Clear values 
+        nameElement.value = "";
+        emailElement.value = "";
+        messageElement.value = "",
+            
+    // Remove contact form elements
+            contactForm.style.display = "none";
+        
+    // Refresh Button
+        refreshButton.style.display = "block";
+    }  
+})
+
+// IMAGE CAROUSEL 
